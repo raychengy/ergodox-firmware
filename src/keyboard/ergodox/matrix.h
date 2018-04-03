07ad4d1750214dd81391b84ea3eca0deec9a5809
Ray Cheng <ray.cheng.y@gmail.com>
2018-04-02 22:46:12 -0400

    Got a working firmware for the dactyl keyboard. WIP - layouts

--- src/keyboard/ergodox/matrix.h
+++ src/keyboard/ergodox/matrix.h
@@ -71,10 +71,10 @@
 													\
 	/* matrix positions */											\
 	{{ k00,k01,k02,k03,k04,k05,na, na,k08,k09,k0A,k0B,k0C,k0D },	\
-	 { k10,k11,k12,k13,k14,na,na, na,k18,k19,k1A,k1B,k1C,k1D },	\
+	 { k10,k11,k12,k13,k14,na,na,  na,k18,k19,k1A,k1B,k1C,k1D },		\
 	 { k20,k21,k22,k23,k24,k25,na, na,k28,k29,k2A,k2B,k2C,k2D },	\
 	 { k30,k31,k32,k33,k34,k35,na, na,k38,k39,k3A,k3B,k3C,k3D },	\
-	 { k40,k41,k42,k43,k44,k45,na, na,k48,k49,k4A,k4B,k4C,na },	\
+	 { k40,k41,k42,k43,k44,k45,na, na,k48,k49,k4A,k4B,k4C,na },		\
 	 { k50,k51,k52,k53,k54,k55,na, na,k58,k59,k5A,k5B,k5C,k5D }}
 
 
